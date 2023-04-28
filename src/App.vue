2a2855fb087adb5712fd52b63520ca41957d0fb4
Aribunfi <arianna@pabis.de>
2023-04-28 16:40:21 +0200

    importata header nella app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script>
 
+import AppHeader from './components/AppHeader.vue';
+
 export default {
   data() {
     return {
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,7 +8,7 @@ export default {
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Vue Boolfolio</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
