ab817b7ad4c38a0fdb8c69441f9c989762d056ae
Aribunfi <arianna@pabis.de>
2023-05-03 08:56:30 +0200

    stampa paginazione in lista progetti

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default {
         axios
         .get('http://127.0.0.1:8000/api/projects')
         .then((response) => {
-            this.projects = response.data;
+            this.projects = response.data.data;
         })
 
       }
