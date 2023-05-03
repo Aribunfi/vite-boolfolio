efb104dba4241fc46d8a53ae7ce51ad146250781
Aribunfi <arianna@pabis.de>
2023-05-03 12:29:15 +0200

    compilazione e registrazione HomePage

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,20 @@
 <script>
-import axios from 'axios';
 
 import AppHeader from './components/AppHeader.vue';
-import ProjectList from './components/ProjectList.vue';
-
+import HomePage from './pages/HomePage.vue';
 
 export default {
   data() {
     return {
       title: "Boolfolio",
-      projects: {
-        data: [],
-        pages: [],
-      },
+   
     };
   },
-    components: { AppHeader, ProjectList },
+    components: { AppHeader, HomePage },
 
-    methods: {
-      fetchProjects() {
-        axios
-        .get('http://localhost:5175/api/projects')
-        .then((response) => {
-            this.projects.data = response.data.data;
-            this.projects.pages = response.data.links;
-        })
+    
 
-      }
-    },
-
-created() {
-  this.fetchProjects();
-}
+   
 
   };
   
@@ -41,8 +24,7 @@ created() {
 
 <template>
   <AppHeader :title="title" />
-  <h1>{{ title }}</h1>
-  <ProjectList :projects="projects.data" :pagination="projects.pagination" title="Most recent" class="my-5"/>
+  <HomePage />
 
 </template>
 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,18 +1,49 @@
 <script>
+import axios from 'axios';
+import ProjectList from './components/ProjectList.vue';
 
 export default {
     data() {
         return {
-            title: "Hello world"
+            projects: {
+        data: [],
+        pages: [],
+      },
         }
-    }
+    },
+    components: { ProjectList },
+
+    emits: ['changePage'],
+
+    methods: {
+      fetchProjects(endpoint = null) {
+        if (!endpoint) endpoint = 'http://127.0.0.1:8000/api/projects';
+        axios.get(endpoint)
+        .then((response) => {
+            this.projects.data = response.data.data;
+            this.projects.pages = response.data.links;
+        })
+
+      }
+    },
+
+created() {
+  this.fetchProjects();
+}
+
 };
 
 </script>
 
 
 <template>
-    <h1>{{ title }}</h1>
+  <ProjectList
+  :projects="projects.data"
+  :pages="projects.pages"
+  title="Most recent"
+  class="my-3"
+  @changePage="fetchProjects"
+  />
 </template>
 
 <style lang="scss" scoped></style>
